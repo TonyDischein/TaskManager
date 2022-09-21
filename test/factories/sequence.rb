118c453e9d48f9deaae5469a764a0f3e5befbc42
Anton Romashkin <tony201994@mail.ru>
2022-09-21 15:18:45 +0400

    Format code

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,19 +1,19 @@
 FactoryBot.define do
-    time = Time.new
+  time = Time.new
 
-    sequence :string, aliases: [:first_name, :last_name, :password, :avatar, :name, :description, :state] do |n|
-      "string#{n}"
-    end
-
-    sequence :email do |n|
-        "person#{n}@example.com"
-    end
-
-    sequence :type do |n|
-        ""
-    end
-
-    sequence :expired_at do |n|
-        time.strftime("%Y-%m-%d")
-    end
+  sequence :string, aliases: [:first_name, :last_name, :password, :avatar, :name, :description, :state] do |n|
+    "string#{n}"
   end
+
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+
+  sequence :type do |_n|
+    ''
+  end
+
+  sequence :expired_at do |_n|
+    time.strftime('%Y-%m-%d')
+  end
+end
