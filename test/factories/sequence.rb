cd23478745c805de431c8fbfbc327d942d89cc3c
Anton Romashkin <tony201994@mail.ru>
2022-09-21 14:59:56 +0400

    Implement test for user model

---
+++ test/factories/sequence.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+    sequence :string, aliases: [:first_name, :last_name, :password, :avatar] do |n|
+      "string#{n}"
+    end
+
+    sequence :email do |n|
+        "person#{n}@example.com"
+    end
+
+    sequence :type do |n|
+        ""
+    end
+  end
