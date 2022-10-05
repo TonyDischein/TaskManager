c19ad69d697b526efb8c7bb454e1ed45d1c5580c
Anton Romashkin <tony201994@mail.ru>
2022-10-05 19:42:23 +0400

    Add MUITheme

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -3,11 +3,15 @@ import React from 'react';
 import store from 'store';
 import { Provider } from 'react-redux';
 import TaskBoard from 'containers/TaskBoard';
+import { MuiThemeProvider } from '@material-ui/core';
+import MUITheme from 'MUITheme/MUITheme';
 
 function App() {
   return (
     <Provider store={store}>
-      <TaskBoard />
+      <MuiThemeProvider theme={MUITheme}>
+        <TaskBoard />
+      </MuiThemeProvider>
     </Provider>
   );
 }
