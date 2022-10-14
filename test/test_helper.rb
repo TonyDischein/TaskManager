65b8890d842651eadb5f3a674e40f4b19159f68d
Anton Romashkin <tony201994@mail.ru>
2022-10-14 17:35:35 +0400

    Add output of incorrect queries during testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,9 +16,11 @@ end
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
+require_relative './bullet_helper'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  include BulletHelper
   include FactoryBot::Syntax::Methods
 
   # Run tests in parallel with specified workers
