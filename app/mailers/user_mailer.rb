45f4577e5f427f2a22f22bb21cb06e402a19458d
Anton Romashkin <tony201994@mail.ru>
2022-10-19 18:06:53 +0400

    Add sending notifications when creating, updating and deleting tasks

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,25 @@
 class UserMailer < ApplicationMailer
+  def task_created
+    user = params[:user]
+    @task = params[:task]
+
+    mail(to: user.email, subject: 'New Task Created')
+  end
+
+  def task_updated
+    @task = params[:task]
+    user = params[:user]
+
+    users = User.where(id: [@task.author_id, @task.assignee_id]).pluck(:email)
+    users |= [user.email]
+
+    mail(to: users, subject: "Task #{@task.id} Updated")
+  end
+
+  def task_deleted
+    user = params[:user]
+    @task = params[:task]
+
+    mail(to: user.email, subject: "Task #{@task.id} Deleted")
+  end
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,7 +1,61 @@
-require "test_helper"
+require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'task created' do
+    user = create(:user)
+    task = create(:task, author: user)
+    params = { user: user, task: task }
+
+    email = UserMailer.with(params).task_created
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['noreply@taskmanager.com'], email.from
+    assert_equal [user.email], email.to
+    assert_equal 'New Task Created', email.subject
+    assert email.body.to_s.include?("Task #{task.id} was created")
+  end
+
+  test 'task update' do
+    user = create(:user)
+    task = create(:task, author: user)
+    assignee = create(:user)
+
+    task_attributes = attributes_for(:task).
+      merge({ author_id: user.id, assignee_id: assignee.id }).
+      stringify_keys
+    task.update(task_attributes)
+
+    params = { user: user, task: task }
+    email = UserMailer.with(params).task_updated
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['noreply@taskmanager.com'], email.from
+    assert_equal [user.email, assignee.email], email.to
+    assert_equal "Task #{task.id} Updated", email.subject
+    assert email.body.to_s.include?("Task #{task.id} was updated")
+  end
+
+  test 'task deleted' do
+    user = create(:user)
+    task = create(:task, author: user)
+    task.destroy
+
+    params = { user: user, task: task }
+    email = UserMailer.with(params).task_deleted
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['noreply@taskmanager.com'], email.from
+    assert_equal [user.email], email.to
+    assert_equal "Task #{task.id} Deleted", email.subject
+    assert email.body.to_s.include?("Task #{task.id} was deleted")
+  end
 end
