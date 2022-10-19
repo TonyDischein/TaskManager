95689175dc0a17ca09bc2f2e629273ff547a80cc
Anton Romashkin <tony201994@mail.ru>
2022-10-19 22:41:57 +0400

    Add layout to emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,25 +1,31 @@
 class UserMailer < ApplicationMailer
   def task_created
-    user = params[:user]
-    @task = params[:task]
+    user, @task = get_message_data
 
     mail(to: user.email, subject: 'New Task Created')
   end
 
   def task_updated
-    @task = params[:task]
-    user = params[:user]
-
-    users = User.where(id: [@task.author_id, @task.assignee_id]).pluck(:email)
-    users |= [user.email]
+    user, @task = get_message_data
+    users = get_recipients_list(user)
 
     mail(to: users, subject: "Task #{@task.id} Updated")
   end
 
   def task_deleted
-    user = params[:user]
-    @task = params[:task]
+    user, @task = get_message_data
 
     mail(to: user.email, subject: "Task #{@task.id} Deleted")
+  end
+
+  private
+
+  def get_message_data
+    [params[:user], params[:task]]
+  end
+
+  def get_recipients_list(user)
+    users = User.where(id: [@task.author_id, @task.assignee_id]).pluck(:email)
+    users | [user.email]
   end
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,3 +1,27 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def task_created
+    params = get_message_data
+
+    UserMailer.with(params).task_created
+  end
+
+  def task_updated
+    params = get_message_data
+
+    UserMailer.with(params).task_updated
+  end
+
+  def task_deleted
+    params = get_message_data
+
+    UserMailer.with(params).task_deleted
+  end
+
+  private
+
+  def get_message_data
+    user = User.first
+    task = Task.first
+    { user: user, task: task }
+  end
 end
