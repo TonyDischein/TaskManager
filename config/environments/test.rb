3ff90b689fc013b3fff90d962f5335067af7cc93
Anton Romashkin <tony201994@mail.ru>
2022-10-21 20:58:21 +0400

    Add default_url_options from environment variables

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   config.active_storage.service = :test
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.default_url_options = { host: ENV['TEST_HOST'] }
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
