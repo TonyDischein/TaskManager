df9579135f28ae98d42b07a45a69b201190221eb
Anton Romashkin <tony201994@mail.ru>
2022-10-21 18:22:06 +0400

    Add user models methods for password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
-  has_secure_password
+  include Recoverable
+  has_secure_password validations: false
 
   has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
   has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
 
   validates :first_name, :last_name, presence: true, length: { minimum: 2 }
   validates :email, presence: true, uniqueness: true, format: /@/
+  validates :password, confirmation: true, allow_blank: false
 end
